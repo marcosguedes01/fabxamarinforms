07304b8f12e9c589d2e3f61710bfab1b9a1cf3d9
Marcos Guedes <marcos.guedesjr@hotmail.com>
2017-05-18 00:29:32 -0300

    Add project files.

---
+++ FABExample/FABExample/FabViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace FABExample
+{
+    public class FabViewModel : INotifyPropertyChanged
+    {
+        public Command FabCommand { get; }
+        public ObservableCollection<string> LstItems { get; set; }
+
+        public FabViewModel()
+        {
+            this.FabCommand = new Command(ExecuteFabCommand);
+
+            LstItems = new ObservableCollection<string>();
+            for (int i = 0; i < 30; i++)
+            {
+                LstItems.Add($"Test Line {i}");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private async void ExecuteFabCommand(object obj)
+        {
+            await Application.Current.MainPage.DisplayAlert("Fab Example", "Executando da ViewModel", "OK");
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ FABExample/FABExample/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FABExample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Fab.Clicked = async (sender, args) =>
+            //{
+            //    await this.DisplayAlert("Fab Example", "Executando click!", "OK");
+            //};
+
+            this.BindingContext = new FabViewModel();
+        }
+
+        public Command FabExecuteCommand
+        {
+            get
+            {
+                return new Command(async () => await OnFabExecuted());
+            }
+        }
+
+        private async Task OnFabExecuted()
+        {
+            await DisplayAlert("Fab", "Hide and show the Fab?", "OK");
+        }
+    }
+}
